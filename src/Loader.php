3c6e1ec629d782a79fd6d8d7f57d427f77534ad1
Daniel S. Deboer <daniel.s.deboer@gmail.com>
2020-05-01 15:20:50 -0400

    Initial commit

---
+++ src/Loader.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Dbt\PhpCsFixerConfig;
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+final class Loader
+{
+    /** @var bool */
+    private $allowRisky = true;
+
+    /** @var \PhpCsFixer\Finder */
+    private $finder;
+
+    /** @var array */
+    private $rules;
+
+    public function __construct (Finder $finder, array $rules = [])
+    {
+        $this->finder = $finder;
+        $this->rules = $rules;
+    }
+
+    public function getConfig (): Config
+    {
+        return Config::create()
+            ->setFinder($this->getFinder())
+            ->setRiskyAllowed($this->getAllowRisky())
+            ->setRules($this->getRules());
+    }
+
+    public function allowRisky (): void
+    {
+        $this->allowRisky = true;
+    }
+
+    public function disallowRisky (): void
+    {
+        $this->allowRisky = false;
+    }
+
+    private function getAllowRisky (): bool
+    {
+        return $this->allowRisky;
+    }
+
+    private function getFinder (): Finder
+    {
+        return $this->finder;
+    }
+
+    private function getRules (): array
+    {
+        $rulesFromFile = require __DIR__ . '/rules.php';
+
+        return array_merge($rulesFromFile, $this->rules);
+    }
+}
